77f3f32cd750a33849a8c0bc966273d9feb8bf4f
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2022-08-13 18:42:24 +0600

    easy browser done

---
+++ EasyBrowser/BasicViewController.swift
@@ -0,0 +1,101 @@
+//
+//  BasicViewController.swift
+//  EasyBrowser
+//
+//  Created by Shah Md Imran Hossain on 13/8/22.
+//
+
+import UIKit
+import WebKit
+
+class BasicViewController: UIViewController {
+    var webView: WKWebView!
+    var progressView: UIProgressView!
+    var websites = ["apple.com", "shahcodersden.com", "hackingwithswift.com", "google.com"]
+
+    // load view get called before view did load
+    override func loadView() {
+        super.loadView()
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        let backButton = UIBarButtonItem(image: UIImage(systemName: "chevron.left"), style: .plain, target: webView, action: #selector(webView.goBack))
+        let forwardButton = UIBarButtonItem(image: UIImage(systemName: "chevron.right"), style: .plain, target: webView, action: #selector(webView.goForward))
+        
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
+        
+        toolbarItems = [backButton, spacer, progressButton, spacer, forwardButton, spacer, refresh]
+        navigationController?.isToolbarHidden = false
+        
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+        
+        let url = URL(string: "https://" + websites[3])!
+        webView.load(URLRequest(url: url))
+        // allows the web view to travel forward and backward
+        webView.allowsBackForwardNavigationGestures = true
+    }
+    
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        guard let actionTitle = action.title else {
+            print("Action title is nil")
+            return
+        }
+        
+        let url = URL(string: "https://" + actionTitle)!
+        webView.load(URLRequest(url: url))
+        // allows the web view to travel forward and backward
+        webView.allowsBackForwardNavigationGestures = true
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
+}
+
+extension BasicViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        
+        if let host = url?.host {
+            for website in websites {
+                if host.contains(website) {
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+        
+        decisionHandler(.cancel)
+    }
+}
+
---
+++ readme.md
@@ -0,0 +1,10 @@
+# In this project we learn
+
+## LoadView
+## WKWebView
+## Delegation
+## URL
+## URLRequest
+## UIToolbar
+## UIProgressView
+## Key-Value Observing
